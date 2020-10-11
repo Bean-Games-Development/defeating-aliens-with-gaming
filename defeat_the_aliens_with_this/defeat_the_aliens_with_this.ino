2044b30b92298fca9a2ab6f299921d4a451eba07
Bean-Games-Development <72340559+Bean-Games-Development@users.noreply.github.com>
2020-10-11 22:22:30 +1100

    Update defeat_the_aliens_with_this.ino

--- defeat_the_aliens_with_this/defeat_the_aliens_with_this.ino
+++ defeat_the_aliens_with_this/defeat_the_aliens_with_this.ino
@@ -1,47 +1,7 @@
-  /*--------------------------------------------------------------------------------------
 
-  dmd_test.cpp 
-    Demo and example project for the Freetronics DMD, a 512 LED matrix display
-    panel arranged in a 32 x 16 layout.
-
-  Copyright (C) 2011 Marc Alexander (info <at> freetronics <dot> com)
-
-  See http://www.freetronics.com/dmd for resources and a getting started guide.
-
-  Note that the DMD library uses the SPI port for the fastest, low overhead writing to the
-  display. Keep an eye on conflicts if there are any other devices running from the same
-  SPI port, and that the chip select on those devices is correctly set to be inactive
-  when the DMD is being written to.
-
-  USAGE NOTES
-  -----------
-
-  - Place the DMD library folder into the "arduino/libraries/" folder of your Arduino installation.
-  - Get the TimerOne library from here: http://code.google.com/p/arduino-timerone/downloads/list
-    or download the local copy from the DMD library page (which may be older but was used for this creation)
-    and place the TimerOne library folder into the "arduino/libraries/" folder of your Arduino installation.
-  - Restart the IDE.
-  - In the Arduino IDE, you can open File > Examples > DMD > dmd_demo, or dmd_clock_readout, and get it
-    running straight away!
-
-  * The DMD comes with a pre-made data cable and DMDCON connector board so you can plug-and-play straight
-    into any regular size Arduino Board (Uno, Freetronics Eleven, EtherTen, USBDroid, etc)
-    
-  * Please note that the Mega boards have SPI on different pins, so this library does not currently support
-    the DMDCON connector board for direct connection to Mega's, please jumper the DMDCON pins to the
-    matching SPI pins on the other header on the Mega boards.
-
-  This example code is in the public domain.
-  The DMD library is open source (GPL), for more see DMD.cpp and DMD.h
-
-  --------------------------------------------------------------------------------------*/
-
-  /*--------------------------------------------------------------------------------------
-    Includes
-  --------------------------------------------------------------------------------------*/
-  #include <SPI.h>        //SPI.h must be included as DMD is written by SPI (the IDE complains otherwise)
-  #include <DMD.h>        //
-  #include <TimerOne.h>   //
+  #include <SPI.h>
+  #include <DMD.h>
+  #include <TimerOne.h>
   #include "SystemFont5x7.h"
   #include "Arial_black_16.h"
 
